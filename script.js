6d7e77ef0b00a543e6cbfd038b1df20034b2bcdc
blacr7 <blacr7@mail.broward.edu>
2019-03-27 18:56:25 -0400

    Janique Mynot Realty Redesign.
    Added Navbar, removed images, and changed layout and design for smaller screens.

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementsByTagName("nav")[0].style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementsByTagName("nav")[0].style.display = "inline";
+    document.getElementsByTagName("nav")[0].style.width = "0%";
+}
